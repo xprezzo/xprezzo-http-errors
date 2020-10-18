26bbcd3080b8c7843a2931364c0e5d96cca419fd
Ben Ajenoui <info@seohero.io>
2020-10-18 22:48:08 +0000

    update depds

---
+++ index.js
@@ -0,0 +1,288 @@
+/*!
+ * xprezzo-http-errors
+ * Copyright(c) 2020 Ben Ajenoui <info@seohero.io>
+ * MIT Licensed
+ */
+
+'use strict'
+
+/**
+ * Module dependencies.
+ * @private
+ */
+
+const deprecate = require('depd')('xprezzo-http-errors')
+const setPrototypeOf = require('xprezzo-setprototypeof')
+const statuses = require('statuses')
+const inherits = require('util').inherits
+
+/**
+ * Trasform the given string into a JavaScript identifier
+ *
+ * @param {string} str
+ * @returns {string}
+ * @private
+ */
+const toIdentifier = (str) => {
+  return str
+    .split(/\s/)
+    .map(function (token) {
+      return token.slice(0, 1).toUpperCase() + token.slice(1)
+    })
+    .join('')
+    .replace(/[^ _0-9a-z]/gi, '')
+}
+
+/**
+ * Get the code class of a status code.
+ * @private
+ */
+const codeClass = (status) => {
+  return Number(String(status).charAt(0) + '00')
+}
+
+/**
+ * Create HTTP error abstract base class.
+ * @private
+ */
+const createHttpErrorConstructor= () => {
+  function HttpError () {
+    throw new TypeError('cannot construct abstract class')
+  }
+  inherits(HttpError, Error)
+  return HttpError
+}
+
+/**
+ * Create a new HTTP Error.
+ *
+ * @returns {Error}
+ * @public
+ */
+const createError=function () {
+  let err
+  let msg
+  let status = 500
+  let props = {}
+  for (let i = 0; i < arguments.length; i++) {
+    let arg = arguments[i]
+    if (arg instanceof Error) {
+      err = arg
+      status = err.status || err.statusCode || status
+      continue
+    }
+    switch (typeof arg) {
+      case 'string':
+        msg = arg
+        break
+      case 'number':
+        status = arg
+        if (i !== 0) {
+          deprecate('non-first-argument status code; replace with createError(' + arg + ', ...)')
+        }
+        break
+      case 'object':
+        props = arg
+        break
+    }
+  }
+
+  if (typeof status === 'number' && (status < 400 || status >= 600)) {
+    deprecate('non-error status code; use only 4xx or 5xx status codes')
+  }
+
+  if (typeof status !== 'number' ||
+    (!statuses.message[status] && (status < 400 || status >= 600))) {
+    status = 500
+  }
+
+  // constructor
+  let HttpError = createError[status] || createError[codeClass(status)]
+
+  if (!err) {
+    // create error
+    err = HttpError
+      ? new HttpError(msg)
+      : new Error(msg || statuses.message[status])
+    Error.captureStackTrace(err, createError)
+  }
+
+  if (!HttpError || !(err instanceof HttpError) || err.status !== status) {
+    // add properties to generic error
+    err.expose = status < 500
+    err.status = err.statusCode = status
+  }
+
+  for (let key in props) {
+    if (key !== 'status' && key !== 'statusCode') {
+      err[key] = props[key]
+    }
+  }
+  return err
+}
+
+
+/**
+ * Set the name of a function, if possible.
+ * @private
+ */
+const nameFunc = (func, name) => {
+  let desc = Object.getOwnPropertyDescriptor(func, 'name')
+  /* istanbul ignore next */
+  if (desc && desc.configurable) {
+    desc.value = name
+    Object.defineProperty(func, 'name', desc)
+  }
+}
+
+/**
+ * Get a class name from a name identifier.
+ * @private
+ */
+const toClassName = (name) => {
+  return name.substr(-5) !== 'Error'
+    ? name + 'Error'
+    : name
+}
+
+/**
+ * Create a constructor for a client error.
+ * @private
+ */
+const createClientErrorConstructor = (HttpError, name, code) => {
+  let className = toClassName(name)
+
+  function ClientError (message) {
+    // create the error object
+    let msg = message != null ? message : statuses.message[code]
+    let err = new Error(msg)
+
+    // capture a stack trace to the construction point
+    Error.captureStackTrace(err, ClientError)
+
+    // adjust the [[Prototype]]
+    setPrototypeOf(err, ClientError.prototype)
+
+    // redefine the error message
+    Object.defineProperty(err, 'message', {
+      enumerable: true,
+      configurable: true,
+      value: msg,
+      writable: true
+    })
+
+    // redefine the error name
+    Object.defineProperty(err, 'name', {
+      enumerable: false,
+      configurable: true,
+      value: className,
+      writable: true
+    })
+
+    return err
+  }
+
+  inherits(ClientError, HttpError)
+  nameFunc(ClientError, className)
+
+  ClientError.prototype.status = code
+  ClientError.prototype.statusCode = code
+  ClientError.prototype.expose = true
+
+  return ClientError
+}
+
+/**
+ * Create a constructor for a server error.
+ * @private
+ */
+const createServerErrorConstructor = (HttpError, name, code) => {
+  let className = toClassName(name)
+
+  function ServerError (message) {
+    // create the error object
+    let msg = message != null ? message : statuses.message[code]
+    let err = new Error(msg)
+
+    // capture a stack trace to the construction point
+    Error.captureStackTrace(err, ServerError)
+
+    // adjust the [[Prototype]]
+    setPrototypeOf(err, ServerError.prototype)
+
+    // redefine the error message
+    Object.defineProperty(err, 'message', {
+      enumerable: true,
+      configurable: true,
+      value: msg,
+      writable: true
+    })
+
+    // redefine the error name
+    Object.defineProperty(err, 'name', {
+      enumerable: false,
+      configurable: true,
+      value: className,
+      writable: true
+    })
+
+    return err
+  }
+
+  inherits(ServerError, HttpError)
+  nameFunc(ServerError, className)
+
+  ServerError.prototype.status = code
+  ServerError.prototype.statusCode = code
+  ServerError.prototype.expose = false
+
+  return ServerError
+}
+
+/**
+ * Populate the exports object with constructors for every error class.
+ * @private
+ */
+const populateConstructorExports = (codes) => {
+  codes.forEach(function forEachCode (code) {
+    let CodeError
+    let name = toIdentifier(statuses.message[code])
+
+    switch (codeClass(code)) {
+      case 400:
+        CodeError = createClientErrorConstructor(module.exports.HttpError, name, code)
+        break
+      case 500:
+        CodeError = createServerErrorConstructor(module.exports.HttpError, name, code)
+        break
+    }
+
+    if (CodeError) {
+      // export the constructor
+      module.exports[code] = CodeError
+      module.exports[name] = CodeError
+    }
+  })
+}
+
+/**
+ * Module exports.
+ * @public
+ */
+module.exports = createError
+module.exports.statuses = statuses
+module.exports.HttpError = createHttpErrorConstructor()
+module.exports.isHttpError = ((HttpError) => {
+  return function isHttpError (val) {
+    if (!val || typeof val !== 'object') {
+      return false
+    }else if (val instanceof HttpError) {
+      return true
+    }
+    return val instanceof Error &&
+      typeof val.expose === 'boolean' &&
+      typeof val.statusCode === 'number' && val.status === val.statusCode
+  }
+})(module.exports.HttpError)
+// Populate exports for all constructors
+populateConstructorExports(statuses.codes)
---
+++ test/test.js
@@ -0,0 +1,411 @@
+
+process.env.NO_DEPRECATION = 'http-errors'
+
+var assert = require('assert')
+var util = require('util')
+
+var createError = require('..')
+
+describe('createError(status)', function () {
+  it('should create error object', function () {
+    assert.ok(util.isError(createError(500))) // eslint-disable-line node/no-deprecated-api
+  })
+
+  describe('when status 300', function () {
+    before(function () {
+      this.error = createError(300)
+    })
+
+    it('should have "message" property of "Multiple Choices"', function () {
+      assert.strictEqual(this.error.message, 'Multiple Choices')
+    })
+
+    it('should have "name" property of "Error"', function () {
+      assert.strictEqual(this.error.name, 'Error')
+    })
+
+    it('should have "status" property of 300', function () {
+      assert.strictEqual(this.error.status, 300)
+    })
+
+    it('should have "statusCode" property of 300', function () {
+      assert.strictEqual(this.error.statusCode, 300)
+    })
+  })
+
+  describe('when status 404', function () {
+    before(function () {
+      this.error = createError(404)
+    })
+
+    it('should have "message" property of "Not Found"', function () {
+      assert.strictEqual(this.error.message, 'Not Found')
+    })
+
+    it('should have "name" property of "NotFoundError"', function () {
+      assert.strictEqual(this.error.name, 'NotFoundError')
+    })
+
+    it('should have "status" property of 404', function () {
+      assert.strictEqual(this.error.status, 404)
+    })
+
+    it('should have "statusCode" property of 404', function () {
+      assert.strictEqual(this.error.statusCode, 404)
+    })
+  })
+
+  describe('when status unknown 4xx', function () {
+    before(function () {
+      this.error = createError(499)
+    })
+
+    it('should have "message" property of "Bad Request"', function () {
+      assert.strictEqual(this.error.message, 'Bad Request')
+    })
+
+    it('should have "name" property of "BadRequestError"', function () {
+      assert.strictEqual(this.error.name, 'BadRequestError')
+    })
+
+    it('should have "status" property with code', function () {
+      assert.strictEqual(this.error.status, 499)
+    })
+
+    it('should have "statusCode" property with code', function () {
+      assert.strictEqual(this.error.statusCode, 499)
+    })
+  })
+
+  describe('when status unknown 5xx', function () {
+    before(function () {
+      this.error = createError(599)
+    })
+
+    it('should have "message" property of "Internal Server Error"', function () {
+      assert.strictEqual(this.error.message, 'Internal Server Error')
+    })
+
+    it('should have "name" property of "InternalServerError"', function () {
+      assert.strictEqual(this.error.name, 'InternalServerError')
+    })
+
+    it('should have "status" property with code', function () {
+      assert.strictEqual(this.error.status, 599)
+    })
+
+    it('should have "statusCode" property with code', function () {
+      assert.strictEqual(this.error.statusCode, 599)
+    })
+  })
+})
+
+describe('createError(status, message)', function () {
+  before(function () {
+    this.error = createError(404, 'missing')
+  })
+
+  it('should create error object', function () {
+    assert.ok(util.isError(this.error)) // eslint-disable-line node/no-deprecated-api
+  })
+
+  it('should have "message" property with message', function () {
+    assert.strictEqual(this.error.message, 'missing')
+  })
+
+  it('should have "status" property with status', function () {
+    assert.strictEqual(this.error.status, 404)
+  })
+
+  it('should have "statusCode" property with status', function () {
+    assert.strictEqual(this.error.statusCode, 404)
+  })
+})
+
+describe('createError.isHttpError(val)', function () {
+  describe('when val is undefined', function () {
+    it('should return false', function () {
+      assert.strictEqual(createError.isHttpError(undefined), false)
+    })
+  })
+
+  describe('when val is null', function () {
+    it('should return false', function () {
+      assert.strictEqual(createError.isHttpError(null), false)
+    })
+  })
+
+  describe('when val is a number', function () {
+    it('should return false', function () {
+      assert.strictEqual(createError.isHttpError(42), false)
+    })
+  })
+
+  describe('when val is a string', function () {
+    it('should return false', function () {
+      assert.strictEqual(createError.isHttpError('foobar'), false)
+    })
+  })
+
+  describe('when val is an empty object', function () {
+    it('should return false', function () {
+      assert.strictEqual(createError.isHttpError({}), false)
+    })
+  })
+
+  describe('when val is a plain Error', function () {
+    it('should return false', function () {
+      assert.strictEqual(createError.isHttpError(new Error()), false)
+    })
+  })
+
+  describe('when val is an instance of HttpError', function () {
+    it('should return true', function () {
+      var err = createError(500)
+
+      assert.ok(err instanceof createError.HttpError)
+      assert.strictEqual(createError.isHttpError(err), true)
+    })
+  })
+
+  describe('when val is an Error passed to createError', function () {
+    it('should return true', function () {
+      var err = createError(500, new Error())
+
+      assert.ok(!(err instanceof createError.HttpError))
+      assert.strictEqual(createError.isHttpError(err), true)
+    })
+  })
+})
+
+describe('HTTP Errors', function () {
+  it('createError(status, props)', function () {
+    var err = createError(404, {
+      id: 1
+    })
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(status, props) with status prop', function () {
+    var err = createError(404, {
+      id: 1,
+      status: 500
+    })
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(status, props) with statusCode prop', function () {
+    var err = createError(404, {
+      id: 1,
+      statusCode: 500
+    })
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(props)', function () {
+    var err = createError({
+      id: 1
+    })
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(msg, status)', function () {
+    var err = createError('LOL', 404)
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+  })
+
+  it('createError(msg)', function () {
+    var err = createError('LOL')
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+  })
+
+  it('createError(msg, props)', function () {
+    var err = createError('LOL', {
+      id: 1
+    })
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(err)', function () {
+    var _err = new Error('LOL')
+    _err.status = 404
+    var err = createError(_err)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.expose, true)
+
+    _err = new Error('LOL')
+    err = createError(_err)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
+
+    err = createError(null)
+    assert.notStrictEqual(err, null)
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
+  })
+
+  it('createError(err) with invalid err.status', function () {
+    var _err = new Error('Connection refused')
+    _err.status = -1
+    var err = createError(_err)
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'Connection refused')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
+  })
+
+  it('createError(err, props)', function () {
+    var _err = new Error('LOL')
+    _err.status = 404
+    var err = createError(_err, {
+      id: 1
+    })
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+    assert.strictEqual(err.expose, true)
+  })
+
+  it('createError(status, err, props)', function () {
+    var _err = new Error('LOL')
+    var err = createError(404, _err, {
+      id: 1
+    })
+    assert.strictEqual(err, _err)
+    assert.strictEqual(err.name, 'Error')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(status, msg, props)', function () {
+    var err = createError(404, 'LOL', {
+      id: 1
+    })
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'LOL')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.id, 1)
+  })
+
+  it('createError(status, msg, { expose: false })', function () {
+    var err = createError(404, 'LOL', {
+      expose: false
+    })
+    assert.strictEqual(err.expose, false)
+  })
+
+  it('new createError.HttpError()', function () {
+    assert.throws(function () {
+      new createError.HttpError() // eslint-disable-line no-new
+    }, /cannot construct abstract class/)
+  })
+
+  it('new createError.NotFound()', function () {
+    var err = new createError.NotFound()
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.expose, true)
+    assert(err.stack)
+  })
+
+  it('new createError.InternalServerError()', function () {
+    var err = new createError.InternalServerError()
+    assert.strictEqual(err.name, 'InternalServerError')
+    assert.strictEqual(err.message, 'Internal Server Error')
+    assert.strictEqual(err.status, 500)
+    assert.strictEqual(err.statusCode, 500)
+    assert.strictEqual(err.expose, false)
+    assert(err.stack)
+  })
+
+  it('new createError["404"]()', function () {
+    var err = new createError['404']()
+    assert.strictEqual(err.name, 'NotFoundError')
+    assert.strictEqual(err.message, 'Not Found')
+    assert.strictEqual(err.status, 404)
+    assert.strictEqual(err.statusCode, 404)
+    assert.strictEqual(err.expose, true)
+    assert(err.stack)
+  })
+
+  it('should preserve error [[Class]]', function () {
+    assert.strictEqual(Object.prototype.toString.call(createError('LOL')), '[object Error]')
+    assert.strictEqual(Object.prototype.toString.call(new createError[404]()), '[object Error]')
+    assert.strictEqual(Object.prototype.toString.call(new createError[500]()), '[object Error]')
+  })
+
+  it('should support err instanceof Error', function () {
+    assert(createError(404) instanceof Error)
+    assert((new createError['404']()) instanceof Error)
+    assert((new createError['500']()) instanceof Error)
+  })
+
+  it('should support err instanceof exposed constructor', function () {
+    assert(createError(404) instanceof createError.NotFound)
+    assert(createError(500) instanceof createError.InternalServerError)
+    assert((new createError['404']()) instanceof createError.NotFound)
+    assert((new createError['500']()) instanceof createError.InternalServerError)
+    assert((new createError.NotFound()) instanceof createError.NotFound)
+    assert((new createError.InternalServerError()) instanceof createError.InternalServerError)
+  })
+
+  it('should support err instanceof HttpError', function () {
+    assert(createError(404) instanceof createError.HttpError)
+    assert((new createError['404']()) instanceof createError.HttpError)
+    assert((new createError['500']()) instanceof createError.HttpError)
+  })
+
+  it('should support util.isError()', function () {
+    /* eslint-disable node/no-deprecated-api */
+    assert(util.isError(createError(404)))
+    assert(util.isError(new createError['404']()))
+    assert(util.isError(new createError['500']()))
+    /* eslint-enable node/no-deprecated-api */
+  })
+})
